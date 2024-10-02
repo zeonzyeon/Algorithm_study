2baa4a2f2285fad7c294a4952aa129252bfd304e
Jihyun Jeon <153730657+zeonzyeon@users.noreply.github.com>
2024-10-02 13:20:22 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.36 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len, int height) {
+    int count = 0;
+    
+    for(int i=0; i<array_len; i++){
+        if(array[i]>height){
+            count++;
+        }
+    }
+    
+    return count;
+}
