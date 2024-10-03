c528d74c5621fc270f8cad8db0bb45563eed9524
Jihyun Jeon <153730657+zeonzyeon@users.noreply.github.com>
2024-10-03 22:16:17 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int solution(const char* message) {
+    int size = strlen(message);
+    
+    return 2 * size;
+}
