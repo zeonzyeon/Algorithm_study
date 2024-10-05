56db091b082dc100f3964ec37e1f13fddcc11aeb
Jihyun Jeon <153730657+zeonzyeon@users.noreply.github.com>
2024-10-05 17:41:49 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.c
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.c
@@ -4,16 +4,14 @@
 
 // num_list_len은 배열 num_list의 길이입니다.
 int solution(int num_list[], size_t num_list_len) {
-    int answer = 0;
+    int answer = -1;
     
     for(int i=0;i<num_list_len;i++){
         if(num_list[i]<0){
             answer = i;
-            return answer;
+            break;
         }
     }
     
-    if(answer==0){
-        return -1;
-    }
+    return answer;
 }
