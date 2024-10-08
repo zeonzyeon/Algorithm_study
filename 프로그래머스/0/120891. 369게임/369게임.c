a833390635cae2972d7fc3afb41178d59986a84f
Jihyun Jeon <153730657+zeonzyeon@users.noreply.github.com>
2024-10-08 14:08:38 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int order) {
+    int count = 0;
+    
+    while(order>0){
+        if(order%10==3||order%10==6||order%10==9){
+            count++;
+        }
+        order/=10;
+    }
+    return count;
+}
